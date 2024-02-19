684dcc76e66f8a3cf925f089e387011276f3ee62
AhmetSari <asari4137@gmail.com>
2024-02-19 19:12:40 +0300

    train added

--- app.py
+++ app.py
@@ -1,39 +1,26 @@
 import streamlit as st
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd 
 import seaborn as sns
-
-
-# keeps the plots in one place. calls image as static pngs
-# matplotlib inline 
 import matplotlib.pyplot as plt # side-stepping mpl backend
-import matplotlib.gridspec as gridspec # subplots
-# import mpld3 as mpl
-
-#Import models from scikit learn module:
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-# from sklearn.cross_validation import KFold   #For K-fold cross validation
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
-"""
-sayfanın gerekli yerlerine başlıklar ekle !!!!!!!!!!!!!!!!!!!!!!!!!!!1 subheader header
+from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from numpy import arange
+
+""" 
+this project is a simple streamlit app that uses the breast cancer dataset to train and evaluate a classifier
+
 """
 
-# try:
-#     df = pd.read_csv("archive/data.csv",header = 0)
-# except:
-#     print("File not found")
 
 class App:
     def __init__(self):
         self.dataset_name = None
-
-        self.Init_Streamlit_Page()
-
         self.df = None
         self.column_names = None
         self.params = dict()
@@ -41,26 +28,27 @@ class App:
         self.X, self.Y = None, None
         self.X_train, self.X_test, self.Y_train, self.Y_test = None, None, None, None  
 
+        self.Init_Streamlit_Page()
+   
     def run(self):
         self.get_dataset()
         self.preprocess_data()
         self.plot_data()
-        # self.add_parameter_ui()
-        # self.generate()
+        self.generate()
 
     def Init_Streamlit_Page(self):
         st.title('Ahmet Sarı YZUP - Streamlit App')
-        
-        # st.subheader('Select Dataset and Classifier')
-
+    
         self.dataset_name = st.sidebar.selectbox(
             'Select Dataset',
-            (['something else','Breast Cancer'])
+            (['Unknown Dataset','Breast Cancer'])
         )
 
-        st.write(f"## {self.dataset_name} Dataset")
-        if self.dataset_name == "something else":
-            st.write("Please select the correct dataset from the dropdown menu")
+        if self.dataset_name == "Unknown Dataset":
+            st.warning("Please select the correct dataset from the dropdown menu")
+        else:
+            st.write(f"## {self.dataset_name} Dataset")
+            
 
         self.classifier_name = st.sidebar.selectbox(
             'Select classifier',
@@ -80,13 +68,77 @@ class App:
             except:
                 st.write("ERROR: File not found")
                 raise FileNotFoundError
+            
+    def _get_classifier(self):
+    
+        if self.classifier_name == 'KNN':
+            try:
+                self.clf = KNeighborsClassifier()
+                param_grid = {'n_neighbors': arange(1, 20, 1)}
 
-    def get_classifier(self):
-        self.classifier_name = st.selectbox("Select Classifier", ["Logistic Regression", "Random Forest", "Decision Tree"])
+                grid_search = GridSearchCV(self.clf, param_grid, cv=10, scoring="accuracy")
+                grid_search = grid_search.fit(self.X_train, self.Y_train)
+                # st.write(f"Best Parameters for KNeighborsClassifier: {grid_search.best_params_}")
 
-    def get_params(self):
-        # get parameters using gridSearch
-        pass
+                self.clf = grid_search.best_estimator_  # En iyi modeli seç
+
+            except Exception as e:
+                st.write(f"Error: {e}")
+
+        elif self.classifier_name == 'SVM':
+            self.clf = SVC(probability=True)  # probability=True, predict_proba kullanmak için
+            param_grid = {'C': [0.1, 1, 10], 'gamma': [0.01, 0.1, 1]}
+            grid_search = GridSearchCV(self.clf, param_grid, cv=5, scoring='accuracy')
+            grid_search.fit(self.X_train, self.Y_train)
+
+            # st.write(f"Best Parameters for SVM: {grid_search.best_params_}")
+
+            # En iyi parametrelerle modeli tekrar eğit
+            self.clf = grid_search.best_estimator_
+    
+        else:
+            self.clf = GaussianNB()
+
+
+    def generate(self):
+        if self.X_test is not None and self.Y_test is not None and self.X_train is not None and self.Y_train is not None:
+            try:
+                self._get_classifier()
+                self.clf.fit(self.X_train, self.Y_train)
+                self._predict_and_evaluate()
+            except Exception as e:
+                st.write(f"Error: {e}")
+     
+    def _predict_and_evaluate(self):
+        try:
+            predictions = self.clf.predict(self.X_test)
+
+            st.title('Model Evaluation Metrics')
+            
+            # Accuracy
+            accuracy = metrics.accuracy_score(self.Y_test, predictions)
+            st.write(f'Accuracy: {accuracy:.4f}')
+
+            # Precision
+            precision = metrics.precision_score(self.Y_test, predictions)
+            st.write(f'Precision: {precision:.4f}')
+
+            # Recall
+            recall = metrics.recall_score(self.Y_test, predictions)
+            st.write(f'Recall: {recall:.4f}')
+
+            # F1-Score
+            f1 = metrics.f1_score(self.Y_test, predictions)
+            st.write(f'F1-Score: {f1:.4f}')
+
+            # Confusion Matrix
+            conf_matrix = confusion_matrix(self.Y_test, predictions)
+            self._plot_conf_matrix(conf_matrix)
+
+        except:
+            st.warning('Please select a dataset and classifier, and make sure the model is trained.')
+
+
 
     def preprocess_data(self):
         if self.dataset_name == "Breast Cancer":
@@ -95,37 +147,40 @@ class App:
 
             self.df.dropna(axis=1, how='all', inplace=True)
 
-            st.write("After removing id and Unnamed: 32 columns from the dataset, the last 10 rows of the dataset are:")
-            st.write(self.df.tail(10))
-            st.write(f"Shape: {self.df.shape}")
-
             for column in self.df.columns:
                 if pd.api.types.is_numeric_dtype(self.df[column].dtype):
                     mean_value = self.df[column].mean()
                     self.df[column].fillna(mean_value, inplace=True)
 
+  
             self.df['diagnosis'] = self.df['diagnosis'].map({'M':1, 'B':0})
             self.X = self.df.drop("diagnosis", axis = 1)
             self.Y = self.df['diagnosis']
 
+            st.subheader("After Preprocessing:")
+            st.write(self.df.tail(10))
+            st.write(f"Shape of dataset: {self.df.shape}")
+
+
             scaler = StandardScaler()
             self.X = scaler.fit_transform(self.X)
 
-            self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.X, self.Y, test_size = 0.3, random_state = 101)
-
-            # st.write("self.x", self.X.shape)
-            # st.write(self.X)
-            # st.write("self.y", self.Y.shape)
-            # st.write(self.Y)
+            self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.X, self.Y, test_size = 0.2, random_state = 101)
 
             st.write(f"X_train: {self.X_train.shape}, X_test: {self.X_test.shape}, Y_train: {self.Y_train.shape}, Y_test: {self.Y_test.shape}")
-            st.write(f"Columns Names: {self.df.columns.values}")
 
-        else:
-            st.write("Something else")
+    def _plot_conf_matrix(self, conf_matrix):
 
-    def generate(self):
-        pass
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False,
+                    xticklabels=['Predicted Benign (0)', 'Predicted Malignant (1)'],
+                    yticklabels=['Actual Benign (0)', 'Actual Malignant (1)'])
+        plt.xlabel('y_pred')
+        plt.ylabel('y_true')
+        st.subheader('Confusion Matrix')
+        st.pyplot(plt)
+
+
 
     def plot_data(self):
         if self.dataset_name == "Breast Cancer":
@@ -133,12 +188,12 @@ class App:
             selected_columns = ['diagnosis', 'radius_mean', 'texture_mean']
             selected_data = self.df[selected_columns]
 
-            st.title('Korelasyon Matrisi ve Scatter Plot')
+            st.subheader('Correlation Matrix and Scatter Plot')
 
             # Korelasyon matrisini çizdir
             correlation_matrix = selected_data.corr()
-            st.subheader('Korelasyon Matrisi')
-            st.write(correlation_matrix)
+            st.subheader('Correlation Matrix')
+            # st.write(correlation_matrix)  
 
             # Heatmap çizdir
             st.pyplot(sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f").figure)
